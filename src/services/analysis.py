bf2502e61aa3b0200a5afac0b6d233af6cf66203
jooniku <joona.nikula@helsinki.fi>
2024-05-22 11:02:10 +0300

    Change the service structure to use the ml models.
    This will allow easier development of the model. It will encapsulate
    the model itself from the app.

--- src/services/analysis.py
+++ src/services/analysis.py
@@ -1,43 +1,17 @@
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-
-
-HOURLY_CUSTOMER_COUNT = "~/Downloads/Tuntikohtainen asiakasmäärä.xlsx"
-BOUGHT_MEALS = "~/Downloads/Kopio_Kumpula asiakasdataa.xlsx"
+from linear_regression import LinearRegressionModel
 
 
 class ModelService:
-    """Class for the statistical model
+    """Class for handling the connection
+    between models and the app.
     """
     def __init__(self, data):
         self.data = data
-        self.model = LinearRegression()
+        self.model = LinearRegressionModel(data=self.data)
 
     def learn(self):
-        self.model.fit(self.data["Daily customers"].to_frame(), self.data["Bought meals"])
+        self.model.learn()
 
     def predict(self, feature):
-        return int(self.model.predict(feature))
-
-hourly_customers = pd.read_excel(io=HOURLY_CUSTOMER_COUNT, skiprows=2, index_col=0)
-bought_meals = pd.read_excel(io=BOUGHT_MEALS, skiprows=2, index_col=0)
-
-cols = ["Chemicum", "Physicum", "Exactum", "All groups"]
-hourly_customers.columns = cols
-bought_meals.columns = cols
-
-hourly_customers = hourly_customers
-
-exa = hourly_customers.Exactum
-
-
-comb = pd.merge(bought_meals.Exactum, hourly_customers.Exactum, on="Date").dropna()
-comb.columns = ["Bought meals", "Daily customers"]
-
-linreg = ModelService(comb)
-
-linreg.learn()
-
-print(linreg.predict(np.array([120]).reshape(-1,1)))
+        return self.model.predict(feature)
 
