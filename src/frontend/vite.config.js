9e017658e76ef00b041005379e0c341319d11a5b
Hamid Aebadi <hamidaebadi@yahoo.com>
2024-06-05 12:34:13 +0300

    Update base path in vit.config.js file

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/fwofront/",
   plugins: [react()],
   preview:{
     port: 8080,
-    strictPort: true
+    strictPort: true,
   },
 
   server:{
