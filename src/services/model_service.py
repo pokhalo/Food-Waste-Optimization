496eae9146a6ba3c281201e1b1bbd2204409711b
jooniku <joona.nikula@helsinki.fi>
2024-06-06 12:48:56 +0300

    Change analysis name
    Co-authored by anleik <leikasa@gmail.com>

---
+++ src/services/model_service.py
@@ -0,0 +1,63 @@
+from src.repositories.data_repository import data_repository
+from src.services.linear_regression import LinearRegressionModel
+from src.services.neural_network import NeuralNetwork
+
+# run with "poetry run python -m src.services.analysis"
+
+
+class ModelService:
+    """Class for handling the connection
+    between models and the app.
+    """
+    def __init__(self):
+        self.data = data_repository.roll_means()
+        self.model = NeuralNetwork(data=self.data)
+
+    def predict(self, feature):
+        return self.model.predict(feature)
+
+    def test_model(self):
+        self.model.learn()
+        return self.model.test()
+        #mse, mae, r2 = self.model.test()
+        #return f"Mean squared error: {mse}\nMean absolute error: {mae}\nR^2: {r2}"
+
+    def fit_and_save(self):
+        try:
+            self.model.fit_and_save()
+            print("Model fitted and saved")
+        except: pass
+
+    def load_model(self):
+        try:
+            self.model.load_model()
+            print("Model loaded")
+        except: pass
+
+# HOW TO USE
+
+def example_model():
+    s = ModelService()
+
+    s.fit_and_save()
+
+    # After defining the class the model must be fitted using
+    
+    s.test_model()
+    
+    # The data is fetched automatically and now it is ready to make predictions
+
+    # Predict using a weekday, e.g. monday = 0, tuesday = 1 ...
+    #print(s.predict(2)) # Prediction is a float representing estimated waste for the given day in kgs
+
+    #print(s.test_model()) # this shows info about the accuracy of the model, does not really work yet
+    
+    #res = []
+    #for i in range(0, 1000):
+    #    day = i % 7
+    #    res.append(s.test_model()[1])
+    #print(sum(res)/1000)
+
+    #return s.predict(2)
+
+print(example_model())
