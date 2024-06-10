379bc2383b4a519707ba3870e204596f1ff9f1b1
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2024-06-10 19:02:50 +0300

    Update schema for restaurant data

--- schemas/restaurants.sql
+++ schemas/restaurants.sql
@@ -6,36 +6,42 @@ CREATE TABLE restaurants (
 CREATE TABLE biowaste (
     date DATE,
     restaurant_id INTEGER REFERENCES restaurants (id),
-    biowaste_customer NUMERIC,
-    biowaste_coffee NUMERIC,
-    biowaste_kitchen NUMERIC,
-    biowaste_hall NUMERIC,
+    biowaste_customer NUMERIC,  -- Asiakasbiojäte
+    biowaste_coffee NUMERIC,    -- Kahvinporot
+    biowaste_kitchen NUMERIC,   -- Muu biojäte, keittiö
+    biowaste_hall NUMERIC,      -- Muu biojäte, sali
     PRIMARY KEY (date, restaurant_id)
 );
 
-CREATE TABLE customers_per_x (
+CREATE TABLE customers_per_hour (
     date DATE,
     hour INTEGER,
     amount INTEGER,
     restaurant_id INTEGER REFERENCES restaurants (id)
 );
 
-CREATE TABLE category (
+CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE
 );
 
+CREATE TABLE sold_lunches_by_category (
+    date DATE,
+    amount NUMERIC,
+    restaurant_id INTEGER REFERENCES restaurants (id),
+    category_id INTEGER REFERENCES categories (id)
+);
+
 CREATE TABLE dishes (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    category_id INTEGER REFERENCES category (id)
+    name TEXT
+    -- category_id INTEGER REFERENCES category (id)
 );
 
 CREATE TABLE sold_lunches (
--- needs more information about source data
-    -- date and time
+    date DATE,
+    amount INTEGER,
     restaurant_id INTEGER REFERENCES restaurants (id),
-    dish_id INTEGER REFERENCES dishes (id),
-    amount INTEGER
+    dish_id INTEGER REFERENCES dishes (id)
 );
 
