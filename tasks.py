718e4161e54f1cf205ba1df38e57421b9497bd6d
Anssi Leikas <128043951+anleik@users.noreply.github.com>
2024-06-14 13:23:16 +0300

    Add windows launch commands

--- tasks.py
+++ tasks.py
@@ -1,25 +1,38 @@
 from invoke import task
+import sys
 
 @task
 def start_development(ctx):
-    ctx.run("FLASK_ENV='development' python3 -m src.app.index", pty=True)
+    if sys.platform != "win32":
+        ctx.run("FLASK_ENV='development' python3 -m src.app.index", pty=True)
+    else:
+        ctx.run("set FLASK_ENV='development' && python -m src.app.index")
 
 @task
 def start_production(ctx):
-    ctx.run("FLASK_ENV='production' python3 -m  src.app.index", pty=True)
-
+    if sys.platform != "win32":
+        ctx.run("FLASK_ENV='production' python3 -m  src.app.index", pty=True)
+    else:
+       ctx.run("set FLASK_ENV='production' && python -m src.app.index") 
 @task
 def start_test_environment(ctx):
-    ctx.run("FLASK_ENV='testing' python3 -m src.app.index", pty=True)
-
+    if sys.platform != "win32":
+        tx.run("FLASK_ENV='testing' python3 -m src.app.index", pty=True)
+    else:
+        ctx.run("set FLASK_ENV='testing' && python -m src.app.index") 
 @task(start_test_environment)
 def test(ctx):
     ctx.run('pytest')
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
-
+    if sys.platform != "win32":
+        ctx.run("coverage run --branch -m pytest", pty=True)
+    else:
+        ctx.run("coverage run --branch -m pytest")
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+    if sys.platform != "win32":
+        ctx.run("coverage html", pty=True)
+    else:
+        ctx.run("coverage html")
