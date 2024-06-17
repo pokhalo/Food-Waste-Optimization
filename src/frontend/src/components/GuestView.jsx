5a5e670e09a81fd6387961f2389ba1806d0b9c3e
helena-ves <helena_ves@proton.me>
2024-06-17 13:49:48 +0300

    Editing buttons and adding Saturday

--- src/frontend/src/components/GuestView.jsx
+++ src/frontend/src/components/GuestView.jsx
@@ -8,10 +8,10 @@ const GuestView = () => {
 
   // Component to present occupancy data of restaurants and estimated amount of biowaste / day / restaurant based on visitor amounts.
   // Indexes in data matrix used to present restaurants: Chemicum - 0, Exactum - 1, Physicum - 2
-  // Indexes in data matrix used to present weekdays: 0 - Monday, 1 - Tuesday, 2 - Wednesday, 3 - Thursday, 4 - Friday
+  // Indexes in data matrix used to present weekdays: 0 - Monday, 1 - Tuesday, 2 - Wednesday, 3 - Thursday, 4 - Friday, 5 - Saturday
 
     const [ fetchedData, setFetchedData ] = useState([])
-    const [ dataToDisplay, setDataToDisplay ] = useState([])
+    const [ occupancyDataToDisplay, setOccupancyDataToDisplay ] = useState([])
     const [ isLoading, setIsLoading ] = useState(true)
     const [ restaurant, setRestaurant ] = useState(0)
     const [ weekday, setWeekday ] = useState(0)
@@ -27,6 +27,7 @@ const GuestView = () => {
     const [ styleForWed, setStyleForWed ] = useState(styleForInactiveButton)
     const [ styleForThu, setStyleForThu ] = useState(styleForInactiveButton)
     const [ styleForFri, setStyleForFri ] = useState(styleForInactiveButton)
+    const [ styleForSat, setStyleForSat ] = useState(styleForInactiveButton)
 
     useEffect(() => {
       let ignore = false
@@ -34,14 +35,14 @@ const GuestView = () => {
         try {
           const response = await requestService.getOccupancyOfRestaurantsByHour()
           if (!ignore) {
-            const chemModified = Object.keys(response.data.Chemicum).map(key => response.data.Chemicum[key]).slice(0, 5)
+            const chemModified = Object.keys(response.data.Chemicum).map(key => response.data.Chemicum[key]).slice(0, 6)
             const chemDropEmptyHours = chemModified.map(arr => arr.slice(9, 16))
-            const exaModified = Object.keys(response.data.Exactum).map(key => response.data.Exactum[key]).slice(0, 5)
+            const exaModified = Object.keys(response.data.Exactum).map(key => response.data.Exactum[key]).slice(0, 6)
             const exaDropEmptyHours = exaModified.map(arr => arr.slice(9, 16))
-            const phyModified = Object.keys(response.data.Physicum).map(key => response.data.Physicum[key]).slice(0, 5)
+            const phyModified = Object.keys(response.data.Physicum).map(key => response.data.Physicum[key]).slice(0, 6)
             const phyDropEmptyHours = phyModified.map(arr => arr.slice(9, 16))  
             setFetchedData([chemDropEmptyHours, exaDropEmptyHours, phyDropEmptyHours])
-            setDataToDisplay(fetchedData)
+            setOccupancyDataToDisplay(fetchedData)
             setIsLoading(false)
           }
         } catch (error) {
@@ -54,13 +55,13 @@ const GuestView = () => {
       }
     }, [])
 
-    ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+    ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
-    const data = {
+    const data_occupancy = {
         labels: dataLabels,
         datasets: [{
             label: 'Estimated occupancy by hour',
-              data: dataToDisplay,
+              data: occupancyDataToDisplay,
               borderWidth: 1
             }]
         }
@@ -73,11 +74,13 @@ const GuestView = () => {
         }
       }
     }
+
+    console.log(fetchedData)
           
       const clickChemicum = () => {
         setTitleForForecast('Estimated Amount of Occupancy, Chemicum')
         setRestaurant(0)
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setOccupancyDataToDisplay(fetchedData[0][weekday])
         setStyleForChem(styleForActiveButton)
         setStyleForExa(styleForInactiveButton)
         setStyleForPhy(styleForInactiveButton)
@@ -85,8 +88,8 @@ const GuestView = () => {
 
       const clickExactum = () => {
         setTitleForForecast('Estimated Amount of Occupancy, Exactum')
-        setRestaurant(1)            
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setRestaurant(1)    
+        setOccupancyDataToDisplay(fetchedData[1][weekday])
         setStyleForExa(styleForActiveButton)
         setStyleForChem(styleForInactiveButton)
         setStyleForPhy(styleForInactiveButton)
@@ -94,61 +97,77 @@ const GuestView = () => {
 
       const clickPhysicum = () => {
         setTitleForForecast('Estimated Amount of Occupancy, Physicum')
-        setRestaurant(2)        
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setRestaurant(2)
+        setOccupancyDataToDisplay(fetchedData[2][weekday])
         setStyleForPhy(styleForActiveButton)
         setStyleForChem(styleForInactiveButton)
         setStyleForExa(styleForInactiveButton)
       }
 
       const clickMon = () => {
-        setWeekday(0)
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setWeekday(0)    
+        setOccupancyDataToDisplay(fetchedData[restaurant][0])
         setStyleForMon(styleForActiveButton)
         setStyleForTue(styleForInactiveButton)
         setStyleForWed(styleForInactiveButton)
         setStyleForThu(styleForInactiveButton)
         setStyleForFri(styleForInactiveButton)
+        setStyleForSat(styleForInactiveButton)        
       }
 
       const clickTue = () => {
-        setWeekday(1)
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setWeekday(1)     
+        setOccupancyDataToDisplay(fetchedData[restaurant][1])
         setStyleForTue(styleForActiveButton)
         setStyleForMon(styleForInactiveButton)
         setStyleForWed(styleForInactiveButton)
         setStyleForThu(styleForInactiveButton)
         setStyleForFri(styleForInactiveButton)
+        setStyleForSat(styleForInactiveButton)       
       }
 
       const clickWed = () => {
         setWeekday(2)
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setOccupancyDataToDisplay(fetchedData[restaurant][2])
         setStyleForWed(styleForActiveButton)
         setStyleForMon(styleForInactiveButton)
         setStyleForTue(styleForInactiveButton)
         setStyleForThu(styleForInactiveButton)
         setStyleForFri(styleForInactiveButton)
+        setStyleForSat(styleForInactiveButton)        
       }
 
       const clickThu = () => {
         setWeekday(3)
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setOccupancyDataToDisplay(fetchedData[restaurant][3])
         setStyleForThu(styleForActiveButton)
         setStyleForMon(styleForInactiveButton)
         setStyleForTue(styleForInactiveButton)
         setStyleForWed(styleForInactiveButton)
         setStyleForFri(styleForInactiveButton)
+        setStyleForSat(styleForInactiveButton)        
       }
 
       const clickFri = () => {
-        setWeekday(4)
-        setDataToDisplay(fetchedData[restaurant][weekday])
+        setWeekday(4)    
+        setOccupancyDataToDisplay(fetchedData[restaurant][4])
         setStyleForFri(styleForActiveButton)
         setStyleForMon(styleForInactiveButton)
         setStyleForTue(styleForInactiveButton)
         setStyleForWed(styleForInactiveButton)
         setStyleForThu(styleForInactiveButton)
+        setStyleForSat(styleForInactiveButton)
+      }
+
+      const clickSat = () => {
+        setWeekday(5)
+        setOccupancyDataToDisplay(fetchedData[restaurant][5])
+        setStyleForSat(styleForActiveButton)
+        setStyleForMon(styleForInactiveButton)
+        setStyleForTue(styleForInactiveButton)
+        setStyleForWed(styleForInactiveButton)
+        setStyleForThu(styleForInactiveButton)   
+        setStyleForFri(styleForInactiveButton)
       }
 
     return (
@@ -158,7 +177,7 @@ const GuestView = () => {
                 <div className="pt-6 pb-6">
                      <div className="p-4"> 
                           <h5 className="title is-5" id="title-of-forecast-1">{titleForForecast}</h5>
-                          <Bar options={options} data={data}></Bar>           
+                          <Bar options={options} data={data_occupancy}></Bar>           
                      </div>
                      <div className="buttons">
                         <button className={styleForChem} onClick={clickChemicum}>Chemicum</button>
@@ -170,7 +189,8 @@ const GuestView = () => {
                         <button className={styleForTue} onClick={clickTue}>Tuesday</button>
                         <button className={styleForWed} onClick={clickWed}>Wednesday</button>
                         <button className={styleForThu} onClick={clickThu}>Thursday</button>
-                        <button className={styleForFri} onClick={clickFri}>Friday</button>                      
+                        <button className={styleForFri} onClick={clickFri}>Friday</button>    
+                        <button className={styleForSat} onClick={clickSat}>Saturday</button>                  
                      </div>
                 </div>
             </div>
