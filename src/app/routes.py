1180f27e7f21ffbd1013eadaef99173f07e248f2
helena-ves <helena_ves@proton.me>
2024-06-18 12:58:18 +0300

    Refactor guestview and fetch data

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,10 +1,15 @@
 from ..app.db import db, init_engine
 from ..repositories import data_repository
+from ..services import model_service
 from ..repositories import db_repository as dbrepo
 from flask import render_template, jsonify, make_response
 import os
 
+model = model_service.ModelService()
+model.load_model()
+
 def init_routes(app):
+
     @app.route("/dbtest")
     def db_conn_test():
         data = data_repository.DataRepository()
@@ -37,6 +42,27 @@ def init_routes(app):
         
     @app.route('/data/occupancy')
     def occupancy_data():
-        data = data_repository.DataRepository().get_average_occupancy()
-        return data
-
+        data = model.predict_occupancy()
+        return jsonify(data)
+    
+    @app.route('/data/biowaste')
+    def biowaste_data():
+        data = model.predict_waste_by_week()
+        data_customer = data["Asiakasbiojäte. tiski (kg) per Kuitti kpl (kg)"]["600 Chemicum"]
+        data_customer = [{ 'Chemicum' : data["Asiakasbiojäte. tiski (kg) per Kuitti kpl (kg)"]["600 Chemicum"] },
+                         { 'Exactum' : data['Asiakasbiojäte. tiski (kg) per Kuitti kpl (kg)']["620 Exactum"]},
+                         { 'Physicum' : data['Asiakasbiojäte. tiski (kg) per Kuitti kpl (kg)']["610 Physicum"]}
+        ]
+        data_coffee = [{ 'Chemicum': data["Biojäte kahvi. porot (kg) per Kuitti kpl (kg)"]["600 Chemicum"] },
+                       { 'Exactum' : data['Biojäte kahvi. porot (kg) per Kuitti kpl (kg)']["620 Exactum"]},
+                       { 'Physicum': data['Biojäte kahvi. porot (kg) per Kuitti kpl (kg)']["610 Physicum"]}
+        ]
+        data_kitchen = [{ 'Chemicum' : data["Keittiön biojäte (ruoanvalmistus) (kg) per Kuitti kpl (kg)"]["600 Chemicum"]},
+                        { 'Exactum' : data['Keittiön biojäte (ruoanvalmistus) (kg) per Kuitti kpl (kg)']["620 Exactum"]},
+                        { 'Physicum' : data['Keittiön biojäte (ruoanvalmistus) (kg) per Kuitti kpl (kg)']["610 Physicum"]}
+        ]
+        data_dining = [{ 'Chemicum': data["Salin biojäte (jämät) (kg) per Kuitti kpl (kg)"]["600 Chemicum"]},
+                       { 'Exactum' : data['Salin biojäte (jämät) (kg) per Kuitti kpl (kg)']["620 Exactum"]},
+                       { 'Physicum' : data['Salin biojäte (jämät) (kg) per Kuitti kpl (kg)']["610 Physicum"]}
+        ]
+        return jsonify({ 'customerBiowaste': data_customer, 'coffeeBiowaste': data_coffee, 'kitchenBiowaste': data_kitchen, 'hallBiowaste': data_dining })
