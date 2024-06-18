03c83c1c6464beb9f584785e00c614674763b75d
jooniku <joona.nikula@helsinki.fi>
2024-06-18 12:01:06 +0300

    Initialize class for database scripts

---
+++ src/repositories/db_scripts.py
@@ -0,0 +1,11 @@
+
+class DatabaseScript:
+    """Class to import data to database.
+    Should not be imported to anything, but
+    run on it's own once to init database.
+    """
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    script = DatabaseScript()
