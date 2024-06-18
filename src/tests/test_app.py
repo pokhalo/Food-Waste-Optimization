0b4a1985717dd676d533d079bb89e9b18d065d7c
ravioli-maul <122291857+ravioli-maul@users.noreply.github.com>
2024-06-18 14:39:24 +0300

    Update test_app.py
    Revert unnecessary changes

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -5,9 +5,6 @@ from src.app.index import app
 
 
 class TestApp(TestCase):
-    # Configure assignent for testing GHA
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    
     def create_app(self):
         return app
 
